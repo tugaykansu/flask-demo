9a399465a16cf4d9f8c7e10f106241549415ad3f
tugaykansu-hypernova <75419337+tugaykansu-hypernova@users.noreply.github.com>
2021-03-31 22:27:58 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home_page():
+    return 'use http://localhost:8000/get_request...'
+
+
+@app.route('/get_request')
+def get_request():
+    asd = ""
+    for key, value in request.args.items():
+        asd += key + "=" + value + "; "
+    return asd
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return "404 not found"
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return "500 internal error"
+
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=8000)
